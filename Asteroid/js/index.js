33d0a3b9f8ae963648e1da43eff77462264f0414
Ammar Ahmed <ammarcola@gmail.com>
2017-08-26 17:57:56 -0400

    cleaned the code a bit

--- Asteroid/js/index.js
+++ Asteroid/js/index.js
@@ -6,7 +6,6 @@ var states = {
     game: "game",
     buttonTest: "buttonTest",
     scoreScreen: "scoreScreen",
-    lockerRoom: "lockerRoom",
 };
 
 var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameDiv');
@@ -16,7 +15,6 @@ game.state.add(states.main, mainState);
 game.state.add(states.game, gameState);
 game.state.add(states.buttonTest, buttonTest);
 game.state.add(states.scoreScreen, scoreScreenState);
-game.state.add(states.lockerRoom, lockerRoomState);
 
 
 game.state.start(states.main);
