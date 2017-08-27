16e2c4917301033e317f6c1b663bb27513c4ffcd
Ammar Ahmed <ammarcola@gmail.com>
2017-08-27 14:18:48 -0400

    test

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const app = express();
-var path = require('path');
-
-
-
-app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port 5000!')
-})
 
 app.get('/', function(req, res){
-  console.log(__dirname + '/index.html');
+ 
   res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static('asteroid'));
 
 app.get('/asteroid', function(req, res){
-  console.log(__dirname + '/asteroid/');
   res.sendFile('/asteroid/', { root: __dirname });
 });
-
-//what ss
-//var path = require('path');
 
