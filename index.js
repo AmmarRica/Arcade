7a435b92532d706a53e219ffbd057f35bfe5ddb3
Ammar Ahmed <ammarcola@gmail.com>
2017-08-26 22:16:27 -0400

    changed port to 5000

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 
 
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 5000, function () {
+  console.log('Example app listening on port 5000!')
 })
 
 app.get('/', function(req, res){
