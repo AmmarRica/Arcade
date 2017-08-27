960a5764d576605f41594c6ea4e9b292db974d79
Ammar <ammarcola@gmail.com>
2017-08-27 15:26:12 -0400

    added listenner.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.listen(process.env.PORT || 3000, function () {
+  console.log('game is listening on listening on port 3000!')
+})
+
+
 app.get('/', function(req, res){
  
   res.sendFile(__dirname + '/index.html');
